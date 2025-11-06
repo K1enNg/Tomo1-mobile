f2b56ac8e369f83ade10521442d489d2f6295b94
K1enNg <hiimkien2702@gmail.com>
2025-11-06 10:42:17 -0500

    fixed typo

--- 01-client/app/Q1.tsx
+++ 01-client/app/Q1.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet, Pressable} from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 import { Image } from "expo-image";
 import React from "react";
 import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from 'react';
-import {useFonts} from "expo-font";
+import { useEffect } from 'react';
+import { useFonts } from "expo-font";
 import { useRouter } from "expo-router";
 
 
@@ -22,7 +22,7 @@ export default function Q1() {
     { id: 9, label: 'Ngón tay', image: require('../assets/images/ngontay.png') },
     { id: 10, label: 'Vai', image: require('../assets/images/vai.png') },
     { id: 11, label: 'Lưng', image: require('../assets/images/lung.png') },
-    { id: 12, label: 'Bụng', image: require('../assets/images/bung.png') }, 
+    { id: 12, label: 'Bụng', image: require('../assets/images/bung.png') },
     { id: 13, label: 'Đầu gối', image: require('../assets/images/daugoi.png') },
     { id: 14, label: 'Bàn chân', image: require('../assets/images/banchan.png') },
   ];
@@ -50,11 +50,11 @@ export default function Q1() {
   return (
     <View style={styles.container}>
 
-       <View>
+      <View>
 
         <Image
           source={require("../assets/images/Group95.png")}
-          style = {{
+          style={{
             width: 360,
             height: 24,
             marginTop: 70
@@ -64,74 +64,74 @@ export default function Q1() {
 
       </View>
 
-      <View style= {{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          width: 340,
-          height: 100
-        }}>
+      <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: 340,
+        height: 100
+      }}>
 
-        <Text style = {[styles.nunitoLight, {marginBottom: 20, fontSize: 18, textAlign: "center"}]}> 
+        <Text style={[styles.nunitoLight, { marginBottom: 20, fontSize: 18, textAlign: "center" }]}>
           Ba mẹ có thể đọc theo các bộ phận cơ thể dưới đây và ấn chọn khi đã đọc bộ phận đó rồi nhé.
         </Text>
 
       </View>
 
-        <View
-        
+      <View
+
+        style={{
+          marginBottom: 30,
+        }}
+      >
+        {
+          rows.map((row, rowIndex) => (
+            <View key={rowIndex} style={styles.row}>
+              {
+                row.map((item) => (
+                  <View key={item.id} style={[styles.card, { flexDirection: "row" }]}>
+                    <Pressable
+                      style={styles.press}
+                      onPress={() => console.log(`Pressed ${item.label}`)}
+                    >
+
+                      <Image
+                        source={item.image}
+                        style={styles.image}
+                      />
+                      <Text style={styles.nunitoSemiBold}>{item.label}</Text>
+
+                    </Pressable>
+                  </View>
+                ))
+              }
+            </View>
+          ))
+        }
+      </View>
+
+      <View style={{
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fcc555",
+        width: 340,
+        height: 50,
+        borderRadius: 20,
+        marginBottom: 30
+      }}>
+
+        <Pressable
           style={{
-            marginBottom: 30,
+            width: 340,
+            height: 30,
+          }}
+          onPress={() => {
+            router.push('/Pause');
           }}
         >
-          {
-            rows.map((row, rowIndex) => (
-              <View key={rowIndex} style={styles.row}>
-                {
-                  row.map((item) => (
-                    <View key={item.id} style={[styles.card, {flexDirection: "row"}]}>
-                      <Pressable
-                        style={styles.press}
-                        onPress={() => console.log(`Pressed ${item.label}`)}
-                        >
-                      
-                        <Image
-                          source={item.image}
-                          style={styles.image}
-                        />
-                        <Text style={styles.nunitoSemiBold}>{item.label}</Text>
 
-                      </Pressable>
-                    </View>
-                  ))
-                }
-              </View>
-            ))          
-          } 
-        </View>
 
-      <View style= {{
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor:"#fcc555",
-          width: 340,
-          height: 50,
-          borderRadius: 20, 
-          marginBottom: 30
-        }}>
-
-         <Pressable
-            style = {{
-              width: 340,
-              height: 30,
-            }}
-            onPress={() => {   
-              router.push('/Pause');
-          }}
-        >
-        
-
-          <Text style = {[styles.nunitoSemiBold, {fontSize: 24, textAlign: "center"}]}>
+          <Text style={[styles.nunitoSemiBold, { fontSize: 24, textAlign: "center" }]}>
             Tiếp Tục
           </Text>
         </Pressable>
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:"#cfeef8"
+    backgroundColor: "#cfeef8"
   },
   nunitoLight: {
     fontFamily: "Nunito-Light",
@@ -158,11 +158,11 @@ const styles = StyleSheet.create({
     color: "#555",
   },
   row: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    marginBottom: 20 
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20
   },
-  card:{
+  card: {
     width: 140,
     height: 55,
     backgroundColor: "white",
@@ -173,14 +173,14 @@ const styles = StyleSheet.create({
     marginBottom: 3,
     paddingHorizontal: 10,
   },
-  image:{
+  image: {
     width: 35,
     height: 35,
     marginBottom: 5,
     marginRight: 10
   },
   press: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     alignItems: "center",
     justifyContent: "flex-start",
     width: 162,
@@ -188,4 +188,4 @@ const styles = StyleSheet.create({
   }
 })
 
-export {Q1};
+export { Q1 };
